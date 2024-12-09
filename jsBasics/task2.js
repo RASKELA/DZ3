ee2e5f098d6c1896e34f08c4123b56d3d241ab93
RASKELA <ss.corobruh@gmail.com>
2024-12-09 20:14:04 +0200

    Update content of task1.js, task2.js, task3.js, and task4.js

--- jsBasics/task2.js
+++ jsBasics/task2.js
@@ -1,10 +1,10 @@
 let name1 = "John";
 let name2 = "Jane";
 
-// Канкатенация строк
+// Concatenation string
 let greetingConcat = "Hello, " + name1 + " and " + name2 + "!";
 console.log(greetingConcat);
 
-// Шаблонная строка
+// Example string
 let greetingTemplate = `Hello, ${name1} and ${name2}!`;
 console.log(greetingTemplate);
--- jsBasics/task4.js
+++ jsBasics/task4.js
@@ -1,15 +1,15 @@
-// Задание 4.1
+// EX 4.1
 let radiusCircle = 5;
 let areaCircle = Math.PI * Math.pow(radiusCircle, 2);
 console.log("Area of circle:", areaCircle.toFixed(2));
 
-// Задание 4.2
+// EX 4.2
 let lengthRectangle = 10;
 let widthRectangle = 4;
 let areaRectangle = lengthRectangle * widthRectangle;
 console.log("Area of rectangle:", areaRectangle.toFixed(2));
 
-// Задание 4.3
+// EX 4.3
 let radiusCylinder = 3;
 let heightCylinder = 7;
 let volumeCylinder = Math.PI * Math.pow(radiusCylinder, 2) * heightCylinder;
